8cbbc7a2b728f9923c444975ea2b4ae6da6e96eb
phuduy <chauphuduy2333@gmail.com>
2024-08-22 10:49:27 +0700

    upload code

--- truycapcamera.py
+++ truycapcamera.py
@@ -2,6 +2,7 @@ import cv2
 import pytesseract
 import os
 import re
+import numpy as np  # Thêm numpy để làm sắc nét ảnh
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\\Tesseract OCR\\tesseract.exe'
 
@@ -18,16 +19,19 @@ while True:
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
+    # Hiển thị hình ảnh đã tiền xử lý
+    cv2.imshow("Gray Image", gray)
+    cv2.imshow("Threshold Image", thresh)
+
     text = pytesseract.image_to_string(thresh, config='--psm 6')
-    print("Text detected:", text)  
+    print("Text detected:", repr(text))
 
     if re.match(r'^\d+(\.\d+)?$', text.strip()):
         image_path = os.path.join(save_directory, f'image{image_counter}.jpg')
-        cv2.imwrite(image_path, frame)  
+        cv2.imwrite(image_path, frame)
         print(f'Đã lưu ảnh: {image_path}')
         break  
 
